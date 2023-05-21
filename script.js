ae81147a2401d6387f8b54c2f0ca12d786780527
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-05-21 15:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
+	let form  = document.getElemntById("form1");
+	let name = document.getElementByName("fname").value;
+	let lname = document.getElementByName("lname").value;
+
+	alert(name+lname);
+
+	
     //Write your code here
 
 }
